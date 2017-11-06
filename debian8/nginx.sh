72cea1c8b3fd27e7f3897b11cc963de45ef8927c
Gemmers0n <matthias.vangemmern@gmail.com>
2017-11-06 15:11:59 +0100

    Create nginx.sh

---
+++ debian8/nginx.sh
@@ -0,0 +1,24 @@
+#NGINX CONFIGURATION
+#Matthias van Gemmern
+#2017-11-06
+
+. nginx.conf
+apt-get update
+apt-get install nginx -y
+
+
+mkdir /etc/nginx/ssl
+##todo quiet generating
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/$URL.duckdns.org.key -out /etc/nginx/ssl/$URL.duckdns.org.crt -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com"
+
+
+cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.org
+#Change values
+#number of cores "worker_processes auto;" todo if or nproc ... currently one core for older rpi
+sed -i.bak 's/\(worker_processes \).*/\11\;/' /etc/nginx/nginx.conf
+sed -i.bak 's/\(ssl_protocols \).*/\1TLSv1\.2\;/' /etc/nginx/nginx.conf
+sed -i.bak 's/\(gzip \).*/\1off\;/' /etc/nginx/nginx.conf
+#Uncomment line if needed
+#sed -i '/ ssl_protocols /s/^/#/' /etc/nginx/nginx.conf
+systemctl restart nginx
+systemctl enable nginx
