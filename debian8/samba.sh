6a4cfd7b852930cb23b4766719664d4625aaa03a
Gemmers0n <matthias.vangemmern@gmail.com>
2017-11-23 11:41:28 +0100

    Update samba.sh

--- debian8/samba.sh
+++ debian8/samba.sh
@@ -4,20 +4,28 @@
 #2017-11-23
 
 
+#include config
 . samba.conf
 
-
+#install packages
 apt-get update
 apt-get install -y samba
 
+#copy original config
 if [ -f /etc/samba/smb.conf.orig ]
 then
 	  echo "Original config already copied"
 else
 	  cp /etc/samba/smb.conf /etc/samba/smb.conf.orig
 fi
-cat /etc/samba/smb.conf.orig|grep -v "^#"|grep -v "^;"|awk 'NF'| sed '/\[Main\]/Q' > /etc/samba/smb.conf
 
+#generate structure
+useradd $USER --system --no-create-home -s /bin/false
+usermod -L $USER
+(echo "$PASSWORD"; echo "$PASSWORD") | smbpasswd -s
+
+#modify config
+cat /etc/samba/smb.conf.orig|grep -v "^#"|grep -v "^;"|awk 'NF'| sed '/\[Main\]/Q' > /etc/samba/smb.conf
 echo "unix charset = UTF-8" >> /etc/samba/smbd.conf
 echo "dos charset = cp1252" >> /etc/samba/smbd.conf
 echo "mangled names = no" >> /etc/samba/smbd.conf
@@ -34,10 +42,6 @@ echo "write list = $USER" >> /etc/samba/smbd.conf
 echo "valid users = $USER" >> /etc/samba/smbd.conf
 echo "guest ok  = no" >> /etc/samba/smbd.conf
 
-useradd $USER --system --no-create-home -s /bin/false
-usermod -L $USER
-(echo "$PASSWORD"; echo "$PASSWORD") | smbpasswd -s
-
-
+#restart and enable service autostart
 sudo systemctl enable smbd
 sudo systemctl start smbd
