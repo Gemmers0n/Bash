b3b07c75ff1df54eea1b52d6ffe3a5c64f3d2437
Gemmers0n <matthias.vangemmern@gmail.com>
2017-11-23 11:43:50 +0100

    Update letsencrypt_WiP.sh

--- debian8/letsencrypt_WiP.sh
+++ debian8/letsencrypt_WiP.sh
@@ -3,9 +3,10 @@
 #2017-11-23
 
 
+#include config
 . letsencrypt.conf
 
-
+#generate structure
 ##todo variable
 cd /root/
 curl --silent https://raw.githubusercontent.com/srvrco/getssl/master/getssl > getssl ; chmod 700 getssl
@@ -15,10 +16,9 @@ echo "ACL=('/root/letsencrypt_server/.well-known/acme-challenge'" >> /root/.gets
 echo "'/root/letsencrypt_server/.well-known/acme-challenge')" >> /root/.getssl/$DOMAIN/getssl.cfg
 mkdir -p /root/letsencrypt_server/.well-known/acme-challenge
 
-
 service nginx stop
 
-
+#get certificate
 cd /root/letsencrypt_server
 python -m SimpleHTTPServer 80&
 cd ..
@@ -27,5 +27,5 @@ pkill python
 cp .getssl/$DOMAIN/$DOMAIN.* /etc/nginx/ssl/
 cat /root/.getssl/$DOMAIN/$DOMAIN.crt /root/.getssl/$DOMAIN/chain.crt > /etc/nginx/ssl/$DOMAIN.chain.crt
 
-
+# start service
 service nginx start
