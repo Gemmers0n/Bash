42cf06aa33b09bc21590b52b166141181e30890f
Gemmers0n <matthias.vangemmern@gmail.com>
2017-11-23 11:27:26 +0100

    Update samba.sh

--- debian8/samba.sh
+++ debian8/samba.sh
@@ -1,13 +1,21 @@
-. samba.conf
-
 #SAMBA CONFIGURATION
+#tested in debian8
 #Matthias van Gemmern
 #2017-11-06
 
 
+. samba.conf
+
+
 apt-get update
-apt-get install samba -y
-cp /etc/samba/smb.conf /etc/samba/smb.conf.orig
+apt-get install -y samba
+
+if [ -f /etc/samba/smb.conf.orig ]
+then
+	  echo "Original config already copied"
+else
+	  cp /etc/samba/smb.conf /etc/samba/smb.conf.orig
+fi
 cat /etc/samba/smb.conf.orig|grep -v "^#"|grep -v "^;"|awk 'NF'| sed '/\[Main\]/Q' > /etc/samba/smb.conf
 
 echo "unix charset = UTF-8" >> /etc/samba/smbd.conf
@@ -29,6 +37,7 @@ echo "guest ok  = no" >> /etc/samba/smbd.conf
 useradd $USER --system --no-create-home -s /bin/false
 usermod -L $USER
 (echo "$PASSWORD"; echo "$PASSWORD") | smbpasswd -s
+
 
 sudo systemctl enable smbd
 sudo systemctl start smbd
