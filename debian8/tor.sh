e9bde79a0219e39e8b4e733d055e8637546fdb00
Gemmers0n <matthias.vangemmern@gmail.com>
2017-11-23 09:46:08 +0100

    Update tor.sh

--- debian8/tor.sh
+++ debian8/tor.sh
@@ -1,6 +1,12 @@
+#TOR CONFIGURATION
+#Matthias van Gemmern
+#2017-11-23
+
+. tor.conf
+
 apt-get install tor -y
 echo "HiddenServiceDir /var/lib/tor/hidden_service/" > /etc/tor/torrc
-echo "HiddenServicePort 1337 `ip a s eth0 | awk '/inet / {print$2}' | cut -d/ -f1`:1337" >> /etc/tor/torrc
+echo "HiddenServicePort $PORT `ip a s eth0 | awk '/inet / {print$2}' | cut -d/ -f1`:$PORT" >> /etc/tor/torrc
 mkdir /var/lib/tor/hidden_service
 chown debian-tor:debian-tor /var/lib/tor/hidden_service
 chmod 0700 /var/lib/tor/hidden_service
