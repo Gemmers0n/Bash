db5ed5ba79894ec2cd9e8ada69fbaad43213aaa3
Gemmers0n <matthias.vangemmern@gmail.com>
2017-11-23 12:01:40 +0100

    Update firefox_sync_WiP.sh

--- debian8/firefox_sync_WiP.sh
+++ debian8/firefox_sync_WiP.sh
@@ -1,18 +1,33 @@
-apt-get install python-dev git-core python-virtualenv screen -y
+
+#BASIC RPI CONFIGURATION
+#Matthias van Gemmern
+#2017-11-23
+
+
+#include config
+. firefox_sync.conf
+
+#install packages
+apt-get install -y python-dev git-core python-virtualenv screen
+
 cd /opt
-sudo git clone https://github.com/mozilla-services/syncserver
+git clone https://github.com/mozilla-services/syncserver
 cd syncserver
-sudo make build
+make build
 cat /opt/syncserver/syncserver.ini |grep -v "^#"|grep -v "^public_url = " > /opt/syncserver/syncserver.ini.new
 echo "public_url = http://$HOSTNAME.duckdns.org:5000/" >> /opt/syncserver/syncserver.ini.new
-mkdir /external/firefoxsync
-chown firefoxsync:firefoxsync /external/firefoxsync/
 echo "sqluri = sqlite:////external/firefoxsync/file.db" >> /opt/syncserver/syncserver.ini.new
 echo "secret = `head -c 20 /dev/urandom | sha1sum|cut -d' ' -f1`" >> /opt/syncserver/syncserver.ini.new
 echo "allow_new_users = true" >> /opt/syncserver/syncserver.ini.new
+
 useradd firefoxsync -s /bin/false
-sudo chown -R firefoxsync:firefoxsync /opt/syncserver
-sudo ln -s /opt/syncserver/local/bin/pserve /usr/bin/pserve
+chown -R firefoxsync:firefoxsync /opt/syncserver
+ln -s /opt/syncserver/local/bin/pserve /usr/bin/pserve
 mv /opt/syncserver/syncserver.ini.new /opt/syncserver/syncserver.ini
+#database directory
+mkdir /external/firefoxsync
+chown firefoxsync:firefoxsync /external/firefoxsync/
+
+#make crontab file
 echo "@reboot firefoxsync screen -dmS firefoxSyncServer pserve /opt/syncserver/syncserver.ini" >> /etc/cron.d/firefoxsync
 chmod 644 /etc/cron.d/firefoxsync
