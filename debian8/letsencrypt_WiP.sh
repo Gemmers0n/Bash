2e18d05c9c5ccdbff3f10acf80ea107da577240e
Gemmers0n <matthias.vangemmern@gmail.com>
2017-11-23 11:01:25 +0100

    Update letsencrypt_WiP.sh

--- debian8/letsencrypt_WiP.sh
+++ debian8/letsencrypt_WiP.sh
@@ -1,27 +1,26 @@
 #LETSENCRYPT CONFIGURATION
 #Matthias van Gemmern
-#2017-11-06
-#uses https://github.com/srvrco/getssl
+#2017-11-23
 
-LETSENCRYPT_HOME=/opt/letsencrypt
-DOMAIN=gemmers0nkv.duckdns.org
-service nginx stop
+. letsencrypt.conf
 
 ##todo variable
 cd /root/
 curl --silent https://raw.githubusercontent.com/srvrco/getssl/master/getssl > getssl ; chmod 700 getssl
 ./getssl -c gemmers0nkv.duckdns.org
-echo "ACCOUNT_EMAIL=\"matthias.vangemmern@gmail.com\"" >> /root/.getssl/getssl.cfg
-echo "ACL=('/root/letsencrypt_server/.well-known/acme-challenge'" >> /root/.getssl/gemmers0nkv.duckdns.org/getssl.cfg
-echo "'/root/letsencrypt_server/.well-known/acme-challenge')" >> /root/.getssl/gemmers0nkv.duckdns.org/getssl.cfg
+echo "ACCOUNT_EMAIL=\"$MAIL\"" >> /root/.getssl/getssl.cfg
+echo "ACL=('/root/letsencrypt_server/.well-known/acme-challenge'" >> /root/.getssl/$DOMAIN/getssl.cfg
+echo "'/root/letsencrypt_server/.well-known/acme-challenge')" >> /root/.getssl/$DOMAIN/getssl.cfg
 mkdir -p /root/letsencrypt_server/.well-known/acme-challenge
+
+
 service nginx stop
 cd /root/letsencrypt_server
 python -m SimpleHTTPServer 80&
 cd ..
-./getssl gemmers0nkv.duckdns.org
+./getssl $DOMAIN
 pkill python
-cp .getssl/gemmers0nkv.duckdns.org/gemmers0nkv.duckdns.org.* /etc/nginx/ssl/
-cat /root/.getssl/gemmers0nkv.duckdns.org/gemmers0nkv.duckdns.org.crt /root/.getssl/gemmers0nkv.duckdns.org/chain.crt > /etc/nginx/ssl/gemmers0nkv.duckdns.org.chain.crt
-service nginx start
+cp .getssl/$DOMAIN/$DOMAIN.* /etc/nginx/ssl/
+cat /root/.getssl/$DOMAIN/$DOMAIN.crt /root/.getssl/$DOMAIN/chain.crt > /etc/nginx/ssl/$DOMAIN.chain.crt
 
+service nginx start
