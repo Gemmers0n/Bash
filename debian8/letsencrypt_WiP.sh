e5fed1cfb8e5206e2ff6439ff1faf2c3056af116
Gemmers0n <matthias.vangemmern@gmail.com>
2017-11-24 16:11:18 +0100

    Update letsencrypt_WiP.sh

--- debian8/letsencrypt_WiP.sh
+++ debian8/letsencrypt_WiP.sh
@@ -10,10 +10,10 @@
 ##todo variable
 cd /root/
 curl --silent https://raw.githubusercontent.com/srvrco/getssl/master/getssl > getssl ; chmod 700 getssl
-./getssl -c $DOMAIN
+./getssl -c $DOMAIN.duckdns.org
 echo "ACCOUNT_EMAIL=\"$MAIL\"" >> /root/.getssl/getssl.cfg
-echo "ACL=('/root/letsencrypt_server/.well-known/acme-challenge'" >> /root/.getssl/$DOMAIN/getssl.cfg
-echo "'/root/letsencrypt_server/.well-known/acme-challenge')" >> /root/.getssl/$DOMAIN/getssl.cfg
+echo "ACL=('/root/letsencrypt_server/.well-known/acme-challenge'" >> /root/.getssl/$DOMAIN.duckdns.org/getssl.cfg
+echo "'/root/letsencrypt_server/.well-known/acme-challenge')" >> /root/.getssl/$DOMAIN.duckdns.org/getssl.cfg
 mkdir -p /root/letsencrypt_server/.well-known/acme-challenge
 
 service nginx stop
@@ -22,10 +22,10 @@ service nginx stop
 cd /root/letsencrypt_server
 python -m SimpleHTTPServer 80&
 cd ..
-./getssl $DOMAIN
+./getssl $DOMAIN.duckdns.org
 pkill python
-cp .getssl/$DOMAIN/$DOMAIN.* /etc/nginx/ssl/
-cat /root/.getssl/$DOMAIN/$DOMAIN.crt /root/.getssl/$DOMAIN/chain.crt > /etc/nginx/ssl/$DOMAIN.chain.crt
+cp .getssl/$DOMAIN.duckdns.org/$DOMAIN.duckdns.org.* /etc/nginx/ssl/
+cat /root/.getssl/$DOMAIN.duckdns.org/$DOMAIN.duckdns.org.crt /root/.getssl/$DOMAIN.duckdns.org/chain.crt > /etc/nginx/ssl/$DOMAIN.duckdns.org.chain.crt
 
 # start service
 service nginx start
