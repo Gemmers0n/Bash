f95dd3b7d00c1399876d910271706e5b9b7c4ddf
Gemmers0n <matthias.vangemmern@gmail.com>
2017-11-24 16:53:46 +0100

    Update letsencrypt_WiP.sh

--- debian8/letsencrypt_WiP.sh
+++ debian8/letsencrypt_WiP.sh
@@ -16,6 +16,7 @@ echo "ACCOUNT_EMAIL=\"$MAIL\"" >> /root/.getssl/getssl.cfg
 echo "ACL=('/root/letsencrypt_server/.well-known/acme-challenge'" >> /root/.getssl/$URL.duckdns.org/getssl.cfg
 echo "'/root/letsencrypt_server/.well-known/acme-challenge')" >> /root/.getssl/$URL.duckdns.org/getssl.cfg
 mkdir -p /root/letsencrypt_server/.well-known/acme-challenge
+#TODO uncomment real certificate server
 
 service nginx stop
 
