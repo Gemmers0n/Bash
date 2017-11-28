242afd5a116b42d425592451ee67f80836a4d7fb
Gemmers0n <matthias.vangemmern@gmail.com>
2017-11-28 11:15:32 +0100

    Create duckdns.sh

---
+++ debian8/duckdns.sh
@@ -0,0 +1,10 @@
+include ./duckdns.conf
+mkdir /opt/duckdns
+cd /opt/duckdns
+echo 'echo url="$TOKENURL" | curl -k -o /opt/duckdns/duck.log -K -' > duck.sh
+chmod 700 duck.sh
+./duck.sh
+
+
+#TODO in cron file
+echo "*/5 * * * * /opt/duckdns/duck.sh >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
