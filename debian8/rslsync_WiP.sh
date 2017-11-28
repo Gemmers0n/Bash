c3c9fb963a7daa14c2438639d0041777a1b0e2fe
Gemmers0n <matthias.vangemmern@gmail.com>
2017-11-28 10:50:27 +0100

    Create rslsync_WiP.sh

---
+++ debian8/rslsync_WiP.sh
@@ -0,0 +1,30 @@
+#RESILIO SYNC RPI CONFIGURATION
+#only working on RPI 2+
+#Matthias van Gemmern
+#2017-11-28
+
+
+#include config
+#TODO
+#. rslsync.conf
+
+#Install packages and patches
+apt-get update
+
+CONFIG='/etc/resilio-sync/config.json'
+
+echo "deb [arch=armhf] http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | sudo tee /etc/apt/sources.list.d/resilio-sync.list
+wget -qO - https://linux-packages.resilio.com/resilio-sync/key.asc | sudo apt-key add -
+#sudo dpkg --add-architecture armel #RPI1 
+sudo dpkg --add-architecture armhf
+sudo apt-get update
+sudo apt-get install resilio-sync -y
+#sudo apt-get install resilio-sync:armel RPI1
+
+#create config
+echo '{' > $CONFIG ;echo -n '  "device_name": "' >> $CONFIG ;echo -n `hostname` >> $CONFIG;echo '",' >> $CONFIG
+tail -n+5 conf/rslsync.conf >> $CONFIG
+
+#restart and enable service autostart
+sudo systemctl start resilio-sync
+sudo systemctl enable resilio-sync
