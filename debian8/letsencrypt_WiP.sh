471b9cf84651db83f55a9fe27f282a6c8797c0b0
Gemmers0n <matthias.vangemmern@gmail.com>
2017-12-08 12:10:45 +0100

    Update letsencrypt_WiP.sh

--- debian8/letsencrypt_WiP.sh
+++ debian8/letsencrypt_WiP.sh
@@ -6,15 +6,16 @@
 #include config
 . letsencrypt.conf
 
-DOMAIN2=$URL.duckdns.org
+#todo confilcts with $DOMAIN from sub script
+DOMAIN2=$URL.$TLD
 #generate structure
 ##todo variable
 cd /root/
 curl --silent https://raw.githubusercontent.com/srvrco/getssl/master/getssl > getssl ; chmod 700 getssl
 ./getssl -c $DOMAIN2
 echo "ACCOUNT_EMAIL=\"$MAIL\"" >> /root/.getssl/getssl.cfg
-echo "ACL=('/root/letsencrypt_server/.well-known/acme-challenge'" >> /root/.getssl/$URL.duckdns.org/getssl.cfg
-echo "'/root/letsencrypt_server/.well-known/acme-challenge')" >> /root/.getssl/$URL.duckdns.org/getssl.cfg
+echo "ACL=('/root/letsencrypt_server/.well-known/acme-challenge'" >> /root/.getssl/$URL.$TLD/getssl.cfg
+echo "'/root/letsencrypt_server/.well-known/acme-challenge')" >> /root/.getssl/$URL.$TLD/getssl.cfg
 mkdir -p /root/letsencrypt_server/.well-known/acme-challenge
 #TODO uncomment real certificate server
 
@@ -26,8 +27,8 @@ python -m SimpleHTTPServer 80&
 cd ..
 ./getssl $DOMAIN2
 pkill python
-cp .getssl/$URL.duckdns.org/$URL.duckdns.org.* /etc/nginx/ssl/
-cat /root/.getssl/$URL.duckdns.org/$URL.duckdns.org.crt /root/.getssl/$URL.duckdns.org/chain.crt > /etc/nginx/ssl/$URL.duckdns.org.chain.crt
+cp .getssl/$URL.$TLD/$URL.$TLD.* /etc/nginx/ssl/
+cat /root/.getssl/$URL.$TLD/$URL.$TLD.crt /root/.getssl/$URL.$TLD/chain.crt > /etc/nginx/ssl/$URL.$TLD.chain.crt
 
 # start service
 service nginx start
