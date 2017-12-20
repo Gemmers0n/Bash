6c802be00a2ae056a1ded751aea90377263a462d
Gemmers0n <matthias.vangemmern@gmail.com>
2017-12-20 14:44:42 +0100

    Update nginx_WiP.sh

--- debian8/nginx_WiP.sh
+++ debian8/nginx_WiP.sh
@@ -26,8 +26,6 @@ mkdir /etc/nginx/ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/$URL.duckdns.org.key -out /etc/nginx/ssl/$URL.duckdns.org.crt -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com"
 
 #modify config
-##todo number of cores "worker_processes auto;" todo if or nproc ... currently one core for older rpi
-sed -i.bak 's/\(worker_processes \).*/\11\;/' /etc/nginx/nginx.conf
 sed -i.bak 's/\(ssl_protocols \).*/\1TLSv1\.2\;/' /etc/nginx/nginx.conf
 sed -i.bak 's/\(gzip \).*/\1off\;/' /etc/nginx/nginx.conf
 #TODO keepalive_timeout   2; in nginx.conf
