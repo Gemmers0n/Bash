ae178b56af8d46c681fb13e4e47c673ffed37fbf
Gemmers0n <matthias.vangemmern@gmail.com>
2018-05-23 11:42:07 +0200

    Update basic_system.sh

--- debian9/basic_system.sh
+++ debian9/basic_system.sh
@@ -1,4 +1,4 @@
-#BASIC RPI CONFIGURATION
+#BASIC RPI CONFIGURATION DEBIAN9
 #Matthias van Gemmern
 #2018-05-23
 
@@ -11,15 +11,15 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y tor
 
-#Modify default pi account
+#Modify default pi user
 usermod -p $PASSWORDHASH_PI pi
 usermod -s /bin/false pi
 
 #Modify root
 usermod -p $PASSWORDHASH_ROOT root
 
-#Add new Login User
-#Passwort generieren mit: perl -e 'print crypt("DASPASSWORT", "salt"),"\n"'
+#Add new user
+#generate password with: perl -e 'print crypt("THEPASSWORD", "salt"),"\n"'
 useradd -m -p $PASSWORDHASH_USER1 -s /bin/bash -u $USER1_USERID $USER1_USERNAME
 sudo -u $USER1_USERNAME ssh-keygen -t rsa -b 2048 -f /home/$USER1_USERNAME/.ssh/id_rsa -C "notes" -N ''
 echo $USER1_PUBKEY >> /home/$USER1_USERNAME/.ssh/authorized_keys
