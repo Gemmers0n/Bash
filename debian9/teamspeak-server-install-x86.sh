7ff6c7a111824a2fe7bd8b1283e0730ac4118153
Gemmers0n <matthias.vangemmern@gmail.com>
2018-07-24 19:03:25 +0200

    Update teamspeak-server-install-x86.sh

--- debian9/teamspeak-server-install-x86.sh
+++ debian9/teamspeak-server-install-x86.sh
@@ -7,6 +7,8 @@
 
 
 #VARS
+#TEAMSPEAK_VERSION="3.0.13.8"
+#TEAMSPEAK_VERSION="3.1.3"
 TEAMSPEAK_VERSION="3.2.0"
 TEAMSPEAK_INSTALLDIR="/opt"
 TEAMSPEAK_FRESHINSTALL="YES"
@@ -22,11 +24,15 @@ systemctl stop teamspeak3server
 pkill ts3server
 #clear shared memory
 rm -f /dev/shm/7gbhujb54g8z9hu43jre8
+if [ "$TEAMSPEAK_FRESHINSTALL" == "YES" ]; then
+    rm -Rf $TEAMSPEAK_INSTALLDIR/teamspeak-server
+fi
 
+
+#SYSTEMD-SCRIPT
 cat << EOF > /etc/systemd/system/teamspeak3server.service
 [Unit]
 Description=Teamspeak 3 Server
-
 [Service]
 ExecStart=/opt/teamspeak-server/ts3server_startscript.sh start
 ExecStop=/opt/teamspeak-server/ts3server_startscript.sh stop
@@ -34,7 +40,6 @@ PIDFile=/opt/teamspeak-server/ts3server.pid
 Type=forking
 User=teamspeak
 Group=teamspeak
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -44,39 +49,32 @@ sudo systemctl daemon-reload
 sudo systemctl enable teamspeak3server.service
 
 
-
-#SUDO
-cat << EOF > /etc/sudoers.d/teamspeak
-# Cmnd alias specification
-Cmnd_Alias TEAMSPEAK_CMDS = /bin/systemctl start teamspeak3server, /bin/systemctl stop teamspeak3server, /bin/systemctl restart teamspeak3server, /bin/systemctl status teamspeak3server
-
-# User privilege specification
-teamspeak ALL=NOPASSWD: TEAMSPEAK_CMDS
-EOF
-chmod 0440 /etc/sudoers.d/teamspeak
-#TODOecho "includedir /etc/sudoers.d" >> /etc/sudoers
+if [ "$TEAMSPEAK_FRESHINSTALL" == "YES" ]; then
+    #SUDO
+    echo "teamspeak ALL=NOPASSWD: /bin/systemctl start teamspeak3server, /bin/systemctl stop teamspeak3server, /bin/systemctl restart teamspeak3server, /bin$
+fi
 
 
 #INSTALL
+mkdir $TEAMSPEAK_INSTALLDIR/teamspeak-server
 wget http://dl.4players.de/ts/releases/$TEAMSPEAK_VERSION/teamspeak3-server_linux_amd64-$TEAMSPEAK_VERSION.tar.bz2 --directory-prefix=$TEAMSPEAK_INSTALLDIR
-if [ ! -d $TEAMSPEAK_INSTALLDIR/teamspeak-server-$TEAMSPEAK_VERSION/ ]; then
-    mkdir $TEAMSPEAK_INSTALLDIR/teamspeak-server-$TEAMSPEAK_VERSION
-    if [ ! "$TEAMSPEAK_FRESHINSTALL" == "YES" ]; then
-        cp -a $TEAMSPEAK_INSTALLDIR/teamspeak-server $TEAMSPEAK_INSTALLDIR/teamspeak-server-$TEAMSPEAK_VERSION
-    fi
-    touch $TEAMSPEAK_INSTALLDIR/teamspeak-server/lastused-"´date´"
-    tar xfv $TEAMSPEAK_INSTALLDIR/teamspeak3-server_linux_amd64-$TEAMSPEAK_VERSION.tar.bz2 -C $TEAMSPEAK_INSTALLDIR/teamspeak-server-$TEAMSPEAK_VERSION --strip 1
-    ln -snf $TEAMSPEAK_INSTALLDIR/teamspeak-server-$TEAMSPEAK_VERSION $TEAMSPEAK_INSTALLDIR/teamspeak-server
-    chown -R teamspeak:teamspeak $TEAMSPEAK_INSTALLDIR/teamspeak-server-$TEAMSPEAK_VERSION
-    chmod 0770 $TEAMSPEAK_INSTALLDIR/teamspeak-server-$TEAMSPEAK_VERSION
-else
-    echo "This version already exists, exiting..."
-fi
+tar xfvj $TEAMSPEAK_INSTALLDIR/teamspeak3-server_linux_amd64-$TEAMSPEAK_VERSION.tar.bz2 -C $TEAMSPEAK_INSTALLDIR/teamspeak-server --strip 1
+touch $TEAMSPEAK_INSTALLDIR/teamspeak-server/.ts3server_license_accepted
+chown -R teamspeak:teamspeak $TEAMSPEAK_INSTALLDIR/teamspeak-server
+chmod 0770 $TEAMSPEAK_INSTALLDIR/teamspeak-server
+
 
 #CLEANUP
 rm $TEAMSPEAK_INSTALLDIR/teamspeak3-server_linux_amd64-$TEAMSPEAK_VERSION.tar.bz2
 
+
 #SERVICE
+if [ "$TEAMSPEAK_FRESHINSTALL" == "YES" ]; then
+    #may need some rework
+    sudo -H -u teamspeak bash -c '/opt/teamspeak-server/ts3server_startscript.sh start'
+    #go sure ts3 is down
+    pkill ts3server
+    #clear shared memory
+    rm -f /dev/shm/7gbhujb54g8z9hu43jre8
+fi
 systemctl start teamspeak3server
-#this generates your access masterkey (only on fresh installed version)
-#/opt/teamspeak-server/ts3server_startscript.sh start
