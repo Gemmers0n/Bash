df1e3bc8ca307a8d869fa781b0021dc3381fecfa
Gemmers0n <matthias.vangemmern@gmail.com>
2018-07-24 13:11:44 +0200

    Create certbot.sh

---
+++ debian9/certbot.sh
@@ -0,0 +1,14 @@
+#CERTBOT INSTALL AND LETSENCRYPT CERTREQ
+#Matthias van Gemmern
+#2018-07-24
+
+
+#INCLUDE
+. certbot.conf
+
+#INSTALL
+sudo echo "deb http://ftp.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/stretch-backports.list
+sudo apt-get update && sudo apt-get install -y python-certbot-nginx -t stretch-backports
+
+#CONFIG
+sudo certbot --authenticator nginx --installer nginx --domains $DOMAIN --renew-by-default --agree-tos --no-eff-email -m "$EMAIL" --break-my-certs --no-redirect
